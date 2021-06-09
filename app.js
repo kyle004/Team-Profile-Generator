c82f0da6a79aeb91840e9a8b6169fde981c96c55
Kyle B <kyleberman92@gmail.com>
2021-06-08 19:03:45 -0700

    added inquirer prompts to app.js

--- app.js
+++ app.js
@@ -1,16 +1,185 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
+
+let employeeData = []
+
+const initialQuestion = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: 'What is this persons role?',
+      choices: [Manager, Engineer, Intern]
+    }
+  ])
+    .then(answer => {
+      if (answer.role === 'Manager') {
+        managerData()
+      } else if (answer.role === 'Engineer') {
+        engineerData()
+      } else if (answer.role === 'Intern') {
+        internData()
+      }
+      else {
+        console.log('Done')
+        return
+      }
+    })
+}
+
+initialQuestion()
+
+const internData = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'internName',
+      message: 'What is the name of the intern?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is their employee ID?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email address?'
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What school does the intern go too?'
+    },
+    {
+      type: 'confirm',
+      name: 'addCheck',
+      message: 'Do you have any other employees to add?'
+    }
+  ])
+    .then(answers => {
+      const intern = new Intern(answers.interName, answers.id, answers.email, answers.school)
+      employeeData.push(intern)
+      console.log(employeeData)
+
+      if (answers.addCheck) {
+        initialQuestion()
+      } else {
+        let data = render(employeeData)
+        fs.writeFile(outputPath, data, (err) => {
+          if (err) throw err
+          console.log('The File Has Been Saved')
+        })
+      }
+    })
+}
+
+const engineerData = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'engineerName',
+      message: 'What is the name of the engineer?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the engineers employee ID?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is their Github username?'
+    },
+    {
+      type: 'confirm',
+      name: 'addCheck',
+      message: 'Do you have any other employees to add?'
+    }
+  ])
+    .then(answers => {
+      const engineer = new Engineer(answers.engineerName, answers.id, answers.email, answers.github)
+      employeeData.push(engineer)
+
+      console.log(employeeData)
+
+      if (answers.addCheck) {
+        initialQuestion()
+      } else {
+        let data = render(outputPath, data, (err) => {
+          if (err) throw err
+          console.log('File has been saved')
+        })
+      }
+    })
+}
+
+const managerData = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'managerName',
+      message: 'What is the managers name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the managers employee ID?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email address?'
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is the managers office number?'
+    },
+    {
+      type: 'confirm',
+      name: 'addCheck',
+      message: 'Do you have any other employees to add?'
+    }
+  ])
+    .then(answers => {
+      const manager = new Manager(answers.managerName, answers.id, answers.email, answers.officeNumber)
+      employeeData.push(manager)
+
+      console.log(employeeData)
+
+      if (answers.addCheck) {
+        initialQuestion()
+      } else {
+        let data = render(employeeData)
+        fs.writeFile(outputPath, data, (err) => {
+          if (err) throw err
+          console.log('File has been saved')
+        })
+      }
+    })
+}
 
 
+// {
+//   type: '',
+//     name: '',
+//       message: ''
+// }
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
